925327103d507e4ec678331cbe673855675b32ad
renatommiguel <renatommiguel@gmail.com>
2023-01-29 21:02:00 +0000

    change in main.py,

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ for index, row in df.iterrows():
     # header
     pdf.set_font(family='Times', size=24)
     pdf.set_text_color(100, 100, 100)
-    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+    pdf.cell(w=100, h=12, txt=row['Topic'], align='L', ln=1)
     pdf.line(10, 21, 200, 21)
-
-    pdf.ln(260)
+    
     # set footer
+    pdf.ln(260)
     pdf.set_font(family='Times', size=8)
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
@@ -23,6 +23,7 @@ for index, row in df.iterrows():
     for i in range(row['Pages']-1):
         pdf.add_page()
         # set footer
+        pdf.ln(273)
         pdf.set_font(family='Times', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
